cc9dce45d409c84a81649eaa3309596e65788029
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-09-17 17:16:26 -0400

    Add files via upload
    Simulates a six sided dice roll, will expand on later

---
+++ DiceRoll/DiceRoll/Dice.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoll
+{
+    public class Dice
+    {
+        public string openingLogo()
+        {
+            Console.WriteLine("***Welcome to dice roll***");
+            string dice = "  .-------.      ______      \n" +
+                          " /   o   / |    /\\     \\   \n" +
+                          "/_______/o |   /o \\  o  \\  \n" +
+                          "| o     |  |  /   o\\_____\\ \n" +
+                          "|   o   | o/  \\o   /o    /  \n" +
+                          "|     o | /    \\ o/  o  /   \n" +
+                          "'-------'       \\/____o/    \n";
+
+
+            return dice;
+        }
+        public void rollDice()
+        {
+            Random rand = new Random();
+
+            int side = Math.Abs(rand.Next(1, 6));
+            switch (side)
+            {
+
+                case 1:
+                    Console.WriteLine("---------\n" +
+                                      "|       | \n" +
+                                      "|   o   | \n" +
+                                      "|       | \n" +
+                                      "--------- \n");
+                    break;
+                case 2:
+                    Console.WriteLine("----- \n" +
+                                      "|o  | \n" +
+                                      "|   | \n" +
+                                      "|  o| \n" +
+                                      "----- \n");
+                    break;
+                case 3:
+                    Console.WriteLine("----- \n" +
+                                      "|o  | \n" +
+                                      "| o | \n" +
+                                      "|  o| \n" +
+                                      "----- \n");
+                    break;
+                case 4:
+                    Console.WriteLine("----- \n" +
+                                      "|o o| \n" +
+                                      "|   | \n" +
+                                      "|o o| \n" +
+                                      "----- \n");
+                    break;
+                case 5:
+                    Console.WriteLine("----- \n" +
+                                      "|o o| \n" +
+                                      "| o | \n" +
+                                      "|o o| \n" +
+                                      "----- \n");
+                    break;
+                case 6:
+                    Console.WriteLine("-----\n" +
+                                      "|o  o| \n" +
+                                      "|o  o| \n" +
+                                      "|o  o| \n" +
+                                      "-----\n");
+                    break;
+                default:
+                    Console.WriteLine("Fell off the table");
+                    break;
+            }
+        }
+    }
+}
---
+++ DiceRoll/DiceRoll/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+//  Author: Jonathan Scholl
+//  Date: 9/17/2021
+//  Project: DiceRoll
+//  Description: Simple program to simulate six sided dice roll (ouputs picture of dice)
+*/
+
+using System;
+
+namespace DiceRoll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+
+            Console.WriteLine(dice.openingLogo());
+
+            Console.WriteLine("You rolled a: ");
+            dice.rollDice();
+
+        }
+    }
+}

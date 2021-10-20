4c831fad1e3dd5de15da9e0d854ebd0ea59ea80f
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-10-20 13:23:28 -0400

    testing SetValue method

---
+++ Arrays/SetValue.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new array and print it's contents
+            int[] userArray = initializeArray();
+            Console.WriteLine($"\nOriginal Array: {printArray(userArray)}");
+
+            //Copy method
+            int[] copyArray = new int[userArray.Length];
+            Array.Copy(userArray, copyArray, userArray.Length);
+
+            //SetValue method
+            copyArray.SetValue(32, 3);
+
+            //copyArray[2] = 32;
+
+            //print new array
+            Console.WriteLine($"\nCopied Array: {printArray(copyArray)}");
+
+        }
+
+        //create a unique array based on how many elements
+        public static int[] initializeArray()
+        {
+            Console.Write("How many elements: ");
+            var length = int.Parse(Console.ReadLine());
+
+            int[] numArray = new int[length];
+
+            for (int x = 0; x < numArray.Length; x++)
+            {
+                numArray[x] = int.Parse(Console.ReadLine());
+            }
+
+            return numArray;
+        }
+
+        //print the contents of an array
+        public static string printArray(int[] numArray)
+        {
+            return "{ " + String.Join(", ", numArray) + " }";
+        }
+    }
+}

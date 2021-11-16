d50dcabdcc24ae8e856b57de77607691c6813921
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-11-16 14:26:16 -0500

    Create GeneralPractice1.py

---
+++ Python/GeneralPractice1.py
@@ -0,0 +1,20 @@
+"""
+//  Author: Jonathan Scholl
+//  Date: 11/16/2021
+//  Project: Python practice https://www.w3schools.com/python/python_variables.asp
+//  Description: playing with python syntax to learn language
+"""
+x = "hi"
+y = 1
+z = 'c'
+
+a = float(3)
+b = int(3)
+c = str(3)
+
+print("Hello World!")
+print(x)
+print(y)
+print(z)
+
+print(type(a))
